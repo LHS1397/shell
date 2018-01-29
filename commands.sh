3026e3840ab020b5c70f71fb94b213f6d911a4c1
Lovish <lovish1234567890@gmail.com>
2018-01-29 22:53:55 +0530

    adding sh files

---
+++ commands.sh
@@ -0,0 +1,385 @@
+    1  cd /etc
+    2  ls
+    3  cd
+    4  sudo ubuntu-drivers
+    5  sudo ubuntu-drivers autoinstall
+    6  cd /etc/xdg
+    7  ls
+    8  cd autostart
+    9  ls
+   10  sudo mv at-spi-dbus-bus.desktop at-spi-dbus-bus.disable
+   11  ls
+   12  cd
+   13  clear
+   14  sudo apt-get install decilspie
+   15  sudo apt-get install devilspie
+   16  clear
+   17  sudo apt-get install firefox
+   18  sudo apt-get update
+   19  sudo apt-get upgrade
+   20  clear
+   21  sudo apt-get autoremove
+   22  clear
+   23  sudo apt-get update
+   24  sudo apt-get install firefox
+   25  sudo apt-get update
+   26  sudo apt-get install devilspie
+   27  nano  ~/minimize.ds
+   28  pwd
+   29  clear
+   30  cd /etc
+   31  ls
+   32  nano hostname
+   33  sudo chmod 777 hostname
+   34  nano hostname
+   35  sudo service hostname start
+   36  sudo service hostname restart
+   37  sudo service hostname
+   38  sudo service strat hostname
+   39  sudo service start hostname
+   40  cd
+   41  ls -a
+   42  nano .bashrc
+   43  exit
+   44  sudo apt-get update
+   45  sudo apt-get upgrade
+   46  shutdown -P now
+   47  dpkg -l amdgpu-pro
+   48  groups
+   49  sudo usermod -a -G video $LOGNAME
+   50  cd etc
+   51  cd /etc
+   52  ls
+   53  nano host
+   54  nano hosts
+   55  chmod 755 hosts
+   56  sudo chmod 755 hosts
+   57  nano hosts
+   58  eit
+   59  exit
+   60  gksu gedit /etc/X11/xorg.conf
+   61  cd /home/hawx/Downloads/amdgpu-pro-16.40-348864/
+   62  ls
+   63  sudo ./amdgpu-pro-install 
+   64  nano amdgpu-pro-install 
+   65  fg1
+   66  fg 1
+   67  nano amdgpu-pro-install 
+   68  sudo ./amdgpu-pro-install 
+   69  groups
+   70  cd
+   71  sudo apt-get update
+   72  sudo apt-get upgrade
+   73  exit
+   74  off
+   75  sudo amdcccle
+   76  aticonfig
+   77  find . aticonfig
+   78  c
+   79  sudo amdcccle
+   80  cd /etc
+   81  ls
+   82  cd ati
+   83  ls
+   84  clear
+   85  exit
+   86  lspci -k | greo -EA2 'VGA|3D'
+   87  lspci -k | grep -EA2 'VGA|3D'
+   88  dpkg -l amdgpu-pro
+   89  dpkg -l amdgpu
+   90  dpkg -l amd
+   91  off
+   92  lspci
+   93  man radeon
+   94  c
+   95  sudo apt-get update
+   96  sudo apt-get upgrade
+   97  clear
+   98  rfkill list
+   99  man rfkill
+  100  rfkill unblock 1
+  101  rfkill list
+  102  clear
+  103  c
+  104  firrefox
+  105  firefox
+  106  sudo apt-get update
+  107  sudo apt-get upgrade
+  108  sudo apt-get distupdate
+  109  sudo apt-get dist update
+  110  sudo apt-get dist upgrade
+  111  eject
+  112  eject -t
+  113  eject -T
+  114  c
+  115  lspci | grep fglr
+  116  lspci | grep -i fg
+  117  lspci
+  118  man radeon
+  119  clear
+  120  amdcccle
+  121  lspci -k | grep -EA3 'VGA|3D|Display'
+  122  ubuntu-drivers devices  
+  123  sudo apt-get dist-update
+  124  sudo apt-get dist-upgrade
+  125  sudo apt-get install linux-headers-generic build-essential cdbs fakeroot dh-make debhelper debconf libstdc++6 dkms libqtgui4 wget execstack libelfg0 dh-modaliases
+  126  sudo apt-get install linux-headers-generic build-essential cdbs fakeroot dh-make debhelper debconf libstdc++6 dkms libqtgui4 wget execstack dh-modaliases
+  127  sudo ./home/hawx/Downloads/fglrx-15.302/amd-driver-installer-15.302-x86.x86_64.run
+  128  sudo ./Downloads/fglrx-15.302/amd-driver-installer-15.302-x86.x86_64.run
+  129  sudo apt-get install dkms fakeroot lib32gcc1 libc6-i386 libfakeroot libqtcore4
+  130  sudo amdconfig --initial
+  131  sudo apt-get install fglrx 
+  132  apt-file search amdconfig
+  133  cd /usr
+  134  cd lib
+  135  ls | grep fglrx
+  136  cd fglrx
+  137  ls
+  138  cd xorf
+  139  cd xorg
+  140  ls
+  141  cd modules
+  142  ls
+  143  cd drivers
+  144  ls
+  145  nano fglrx_drv.so
+  146  c
+  147  cd -
+  148  cd --
+  149  cd /usr/lib/fglrx
+  150  ls
+  151  nano alt_ld.so.conf
+  152  sudo apt-get install dh-make dh-modaliases execstack libc6-i386 lib32gcc1
+  153  cd
+  154  clear
+  155  sudo apt-get install fglrx fglrx-amdcccle
+  156  sudo amdconfig –adapter=all –initial
+  157  sudo aticonfig –adapter=all –initial
+  158  sudo aticonfig --initial
+  159  history | grep VGS
+  160  history | grep VGA
+  161  lspci -k | grep -EA3 'VGA|3D|Display'
+  162  off
+  163  lspci -k | grep -EA3 'VGA|3D|Display'
+  164  sudo amdconfig -initial -adapter=all
+  165  sudo aticonfig -initial -adapter=all
+  166  sudo aticonfig -adapter=all
+  167  history
+  168  clear
+  169  history | grep fglrx
+  170  sudo apt-get install fglrx fglrx-core fglrx-amdcccle fglrx-dev fglrx-pxpress
+  171  sudo amdconfig
+  172  amdconfig
+  173  sudo apt-get install fglrx-control
+  174  aticonfig --initial
+  175  modprobe fglrx
+  176  fglrxinfo
+  177  sudo apt-get install linux-headers-generic
+  178  sudo apt-get install fglrx-updates
+  179  sudo apt-get install fglrx xvba-va-driver libva-glx1 libva-egl1 vainfo
+  180  sudo apt-get install fglrx libva-glx1 libva-egl1 vainfo
+  181  sudo amdconfig --initial
+  182  sudo amdconfig --adapter=all --initial
+  183  sudo cp /etc/X11/xorg.conf /etc/X11/xorg.conf.BAK
+  184  sudo apt-get install fglrx-updates fglrx-amdcccle-updates
+  185  sudo apt-get install fglrx-updates xvba-va-driver libva-glx1 libva-egl1 vainfo
+  186  fglrxinfo
+  187  gksudo amdcccle
+  188  amdcccle
+  189  gksudo amdcccle
+  190  sudo apt-get install xvba-va-driver libva-glx1 libva-egl1 vainfo
+  191  sudo apt-get installlibva-glx1 libva-egl1 vainfo
+  192  sudo apt-get install libva-glx1 libva-egl1 vainfo
+  193  sudo vainfo
+  194  sudo ln -s /usr/lib/va/drivers/fglrx_drv_video.so /usr/lib/x86_64-linux-gnu/dri/fglrx_drv_video.so
+  195  sudo apt-get update
+  196  sudo apt-get upgrade
+  197  clear
+  198  fglrxinfo
+  199  sudo apt-get purge fglrx*
+  200  sudo apt-get update
+  201  man radeon
+  202  fglrxinfo
+  203  clear
+  204  sudo ./Downloads/fglrx-15.302/amd-driver-installer-15.302-x86.x86_64.run
+  205  sudo apt-get remove fglrx-install.90Xalv
+  206  sudo apt-get install lib32gcc1 libc6-i386
+  207  sudo dpkg --add-architecture i386
+  208  sudo ./Downloads/fglrx-15.302/amd-driver-installer-15.302-x86.x86_64.run
+  209  sudo ./Downloads/fglrx-15.302/check.sh
+  210  clear
+  211  sudo ./Downloads/amdgpu-pro-17.30-465504/amdgpu-pro-install
+  212  nano Downloads/amdgpu-pro-17.30-465504/amdgpu-pro-install
+  213  sudo ./Downloads/amdgpu-pro-17.30-465504/amdgpu-pro-install
+  214  sudo amdconfig –adapter=all –initia
+  215  sudo aticonfig –adapter=all –initia
+  216  reboot
+  217  tracert
+  218  tracer
+  219  trace
+  220  clear
+  221  exit
+  222  off
+  223  lspci
+  224  history
+  225  c
+  226  history
+  227  fglrxinfo
+  228  sudo ./Downloads/amdgpu-pro-17.30-465504/amdgpu-pro-install
+  229  history
+  230  sudo usermod -a -G video $LOGNAME
+  231  ubuntu-drivers devices 
+  232  off
+  233  ls
+  234  cd Downloads
+  235  ls
+  236  cd amdgpu-pro-17.30-465504 
+  237  ;s
+  238  ls
+  239  ./amdgpu-pro-install --compute
+  240  sudo amdconfig --initial
+  241  sudo aticonfig --initial
+  242  clear
+  243  cd
+  244  reboot
+  245  sudo reboot
+  246  sudo apt-get update
+  247  sudo apt-get upgrade
+  248  c
+  249  ssl lovish@127.0.0.1
+  250  ssh lovish@127.0.0.1
+  251  apt-get install openssh-server 
+  252  sudo apt-get install openssh-server 
+  253  ssh lovish@127.0.0.1
+  254  nano /etc/ssh/sshd_config
+  255  chmod 755 /etc/ssh/sshd_config
+  256  sudo chmod 755 /etc/ssh/sshd_config
+  257  nano /etc/ssh/sshd_config
+  258  sudo chmod 777 /etc/ssh/sshd_config
+  259  nano /etc/ssh/sshd_config
+  260  sudo chmod 755 /etc/ssh/sshd_config
+  261  sudo service ssh restart 
+  262  ssh lovish@localhost
+  263  sudo chmod 777 /etc/ssh/sshd_config
+  264  nano /etc/ssh/sshd_config
+  265  clear
+  266  lshw
+  267  apt-get -y install openssh-client 
+  268  sudo apt-get -y install openssh-client 
+  269  sudo  apt-get -y install openssh-server 
+  270  history | grep
+  271  history | grep fglrx
+  272  fglrxinfo
+  273  apt-get autoremove
+  274  sudo apt-get autoremove
+  275  history | grep LOGNAME
+  276  history | grep amd
+  277  hawx@localhost
+  278  ssh hawx@localhost
+  279  history | grep
+  280  history | grep amdgpu
+  281  dpkg -l amdgpu-pro
+  282  cd /Downloads/amdgpu-pro-17.30-465504
+  283  cd Downloads/amdgpu-pro-17.30-465504
+  284  dpkg -l amdgpu-pro
+  285  history | grep LOGNAME
+  286  sudo usermod -a -G video $LOGNAME
+  287  dpkg -l amdgpu-pro
+  288  history | grep amdgpu
+  289  history | grep initial
+  290  sudo aticonfig --initial
+  291  sudo amdconfig --initial
+  292  clear
+  293  sudo reboot
+  294  dpkg -l amdgpu-pro
+  295  fglrxinfo
+  296  history | grep fglrx
+  297  fglrxinfo
+  298  clear
+  299  amdcccle
+  300  lspci
+  301  history | grep VGA
+  302  lspci -k | grep -EA3 'VGA|3D|Display'
+  303  clear
+  304  history | grep amdgpu
+  305  dpkg -l amdgpu-pro
+  306  amdconfig
+  307  aticonfig
+  308  history
+  309  dpkg -l amdgpu-pro
+  310  sudo apt-get install linux-headers-generic
+  311  sudo apt-get install linux-headers-generic build-essential cdbs fakeroot dh-make debhelper debconf libstdc++6 dkms libqtgui4 wget execstack libelfg0 dh-modaliases
+  312  sudo apt-get install linux-headers-generic build-essential cdbs fakeroot dh-make debhelper debconf libstdc++6 dkms libqtgui4 wget execstack dh-modaliases
+  313  amdccle
+  314  amdcccle
+  315  history | grep fglrx
+  316  sudo apt-get purge fglrx*
+  317  dpkg =l amdgpu-pro
+  318  dpkg -k amdgpu-pro
+  319  dpkg -l amdgpu-pro
+  320  exit
+  321  sudo apt-get update
+  322  sudo apt-get upgrade
+  323  exit
+  324  sudo apt-get install wine
+  325  off
+  326  sudo apt-get update
+  327  sudo rm /var/lib/apt/lists/lock
+  328  sudo apt-get update
+  329  sudo apt-get upgrade
+  330  ps -aux | grep apt
+  331  c
+  332  sudo apt-get install git
+  333  git clone https://github.com/LHS1397/cplus
+  334  lw
+  335  ls
+  336  cd cplus
+  337  ls
+  338  atom class.cpp
+  339  dpkh -l amdspu
+  340  dpkg -l amdspu
+  341  dpkg -l amdgpu
+  342  dpkg -l amdgpu-pro
+  343  clear
+  344  cd
+  345  sudo apt install snapd
+  346  snap install --classic anbox-installer && anbox-installer
+  347  sudo snap install --classic anbox-installer && anbox-installer
+  348  sudo snap install --classic anbox-installer
+  349  snap run anbox-installer
+  350  sudo snap run anbox-installer
+  351  snap run anbox-installer
+  352  sudo apt-get install software-properties-common python-software-properties
+  353  c
+  354  snap run anbox-installer
+  355  anbox
+  356  sudo apt-get update
+  357  snap run anbox-installer
+  358  sudo apt-get update
+  359  snap run anbox-installer
+  360  sudo apt-get --fix-missing
+  361  sudo apt--fix-missing
+  362  sudo apt --fix-missing
+  363  sudo apt-get update --fix-missing
+  364  snap run anbox-installer
+  365  clear
+  366  sudo apt-get autoremove
+  367  sudo apt-get update
+  368  sudo apt-get install --fix-missing
+  369  ping google.com
+  370  ping www.google.com
+  371  c
+  372  ifconfig
+  373  dhclient -r wlp2s0
+  374  dhclient wlp2s0
+  375  sudo dhclient wlp2s0
+  376  sudo apt-get update
+  377  find . anbox
+  378  find / anbox
+  379  fg 1
+  380  c
+  381  eit
+  382  exit
+  383  ping google.com
+  384  c
+  385  history > commands.sh
