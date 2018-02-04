4cda0ad6ef1efdde35f43306d2e2b5593c4865a3
Lovish <lovish1234567890@gmail.com>
2018-02-04 21:33:00 +0530

    git.sh for git refrence

---
+++ git.sh
@@ -0,0 +1,161 @@
+  925  git clone https://github.com/avramit/instahack.git
+ 1198  git
+ 1199  history | grep -i git
+ 1200  git show
+ 1202  git show
+ 1203  git add
+ 1204  git commit
+ 1206  git config --global user.email "lovish1234567890@gmail.com"
+ 1207  git config --global user.name "Lovish"
+ 1208  git commit
+ 1209  git add systools.sh
+ 1210  git commit -s -m "adding systools.sh and other commits"
+ 1211  git sho
+ 1212  git show
+ 1213  git status
+ 1214  git push  https://github.com/LHS1397/cplus
+ 1215  git status
+ 1216  git add -
+ 1217  git add -s
+ 1218  git add -A
+ 1219  git status
+ 1220  git push  https://github.com/LHS1397/cplus
+ 1221  git show
+ 1222  git chehckout
+ 1223  git checkout
+ 1224  git push  https://github.com/LHS1397/cplus
+ 1225  git show
+ 1226  git checkout master
+ 1227  git checkout git
+ 1228  git
+ 1230  git branch
+ 1231  git checkout shell
+ 1232  git create shell
+ 1233  git --help
+ 1235  git create shell
+ 1237  git branch
+ 1238  git create branch shell
+ 1239  git create shell
+ 1240  git help branch
+ 1241  git checkout -b shell
+ 1242  git checkout shell
+ 1243  git merge master shell
+ 1244  git merge shell master
+ 1245  git log
+ 1247  git
+ 1248  git help branch
+ 1249  git branch -A
+ 1250  git branch -
+ 1251  git branch -a
+ 1253  git checkot master
+ 1254  git
+ 1281  git show
+ 1283  git show
+ 1285  git show
+ 1286  git status
+ 1287  history | grep git
+ 1289  git branch
+ 1290  git checkout master
+ 1291  git checkout shell
+ 1292  git commit -s
+ 1293  git commit -s -m "commit by shell"
+ 1400  man git
+ 1401  git init
+ 1404  git init
+ 1405  git branch
+ 1406  git-branch
+ 1407  man git
+ 1408  git show
+ 1409  history | grep git
+ 1410  git show
+ 1411  git status
+ 1412  git branch -a
+ 1413  git log
+ 1414  git branch]=
+ 1415  git branch
+ 1416  man git branch
+ 1417  git checkout -d  LC
+ 1418  git checkout -D LC
+ 1419  history | grep git
+ 1420  git checkout -b LC
+ 1421  git branch
+ 1422  git branch -a
+ 1423  git branch -r
+ 1424  git checkout master
+ 1425  git show
+ 1655  git clone https://github.com/torvalds/linux.git
+ 1660  git psuh
+ 1661  git push
+ 1663  git push
+ 1702  git clone https://github.com/EmpireProject/Empire.git
+ 1713  git clone https://github.com/torvalds/linux.git
+ 1717  git commit -m "creating repo"
+ 1718  git logs
+ 1719  git log
+ 1720  git status
+ 1733  git log
+ 1734  git show
+ 1735  git add 
+ 1737  git add -a 
+ 1738  git add -A
+ 1740  git commit
+ 1742  git commit -m "adding two sh files"
+ 1743  git push https://github.com/LHS1397
+ 1745  git push --set-upstream https://github.com/LHS1397 master
+ 1746  git push --set-upstream https://github.com/LHS1397
+ 1747  history | grep git push
+ 1748  history | grep git
+ 1749  git push https://github.com/LHS1397/shell
+ 1750  git push https://github.com/LHS1397
+ 1752  git log
+ 1753  git pull https://github.com/LHS1397
+ 1754  git push https://github.com/LHS1397
+ 1755  git push origin master https://github.com/LHS1397
+ 1756  git push origin https://github.com/LHS1397
+ 1760  git log
+ 1761  git status
+ 1762  git checkout
+ 1763  git branch 
+ 1764  git checkout -b shell-l
+ 1765  git branch
+ 1766  git merge
+ 1767  git merge master
+ 1769  git status
+ 1770  git checkout master
+ 1772  git status
+ 1773  git chekout shell-l
+ 1774  git checkout shell-l
+ 1776  git remote add origin https://github.com/LHS1397
+ 1778  git push origin master https://github.com/LHS1397
+ 1779  git remote -v
+ 1780  git push origin master https://github.com/LHS1397/shell.git
+ 1781  git remote add origin https://github.com/LHS1397/shell.git
+ 1782  git push -u origin master 
+ 1783  git push -u origin master https://github.com/LHS1397/shell.git
+ 1784  git push -u origin master https://github.com/LHS1397
+ 1785  git push -u origin master
+ 1799  git init
+ 1801  git status
+ 1802  git add -A -m "adding sh files"
+ 1803  git add -A
+ 1804  git add -m "adding sh files"
+ 1805  git commit -m "adding sh files"
+ 1806  git checkout -b shell-l
+ 1807  git merge master
+ 1809  git git remote add origin https://github.com/LHS1397/shell.git
+ 1810  git remote add origin https://github.com/LHS1397/shell.git
+ 1811  git push -u origin master
+ 1819  git add -A
+ 1820  git commit -m "adding 2 more"
+ 1821  git push -u origin master
+ 1822  git status
+ 1823  git checkout master
+ 1840  git status
+ 1841  git log
+ 1842  git checkout c1e0f875cc2b201c7c9ad1edd846cb85644a0875
+ 1844  git checkout 44bfc3d148ab3665a44c7bbe665de2c3c798299e
+ 1848  git status
+ 1853  gir clone https://github.com/torvalds/linux.git
+ 1854  git clone https://github.com/torvalds/linux.git
+ 1861  git status
+ 1865  history | grep git >> git.sh
